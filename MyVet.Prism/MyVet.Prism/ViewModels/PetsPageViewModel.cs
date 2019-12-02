7e16dd9815baf2445e7da31bbcdccb5046c5f683
argenis0117 <argenis0117@hotmail.com>
2019-12-02 09:35:25 -0400

    Class 37 finished. PetsItemViewModel added

--- MyVet.Prism/MyVet.Prism/ViewModels/PetsPageViewModel.cs
+++ MyVet.Prism/MyVet.Prism/ViewModels/PetsPageViewModel.cs
@@ -12,14 +12,14 @@ namespace MyVet.Prism.ViewModels
     public class PetsPageViewModel : ViewModelBase
     {
         private OwnerResponse _owner;
-        private ObservableCollection<PetResponse> _pets;
+        private ObservableCollection<PetItemViewModel> _pets;
         public PetsPageViewModel(
-            INavigationService navigationService) :base(navigationService)
+            INavigationService navigationService) : base(navigationService)
         {
             Title = "Pets";
         }
 
-        public ObservableCollection<PetResponse> Pets 
+        public ObservableCollection<PetItemViewModel> Pets
         {
             get => _pets;
             set => SetProperty(ref _pets, value); //Se asigna el valor por setproperty para que lo refresque
@@ -33,7 +33,17 @@ namespace MyVet.Prism.ViewModels
             {
                 _owner = parameters.GetValue<OwnerResponse>("owner");
                 Title = $"Pets of: {_owner.FullName}";
-                Pets = new ObservableCollection<PetResponse>(_owner.Pets);
+                Pets = new ObservableCollection<PetItemViewModel>(_owner.Pets.Select(p => new PetItemViewModel
+                {
+                    Born = p.Born,
+                    Histories = p.Histories,
+                    Id = p.Id,
+                    ImageUrl = p.ImageUrl,
+                    Name = p.Name,
+                    PetType = p.PetType,
+                    Race = p.Race,
+                    Remarks = p.Remarks
+                }).ToList());
             }
         }
     }
