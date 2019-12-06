b90f6afa44c1a2b148698651c8bcaa2e375c0576
argenisr0117 <argenis0117@hotmail.com>
2019-12-06 15:30:31 -0400

    Class 46 finished. Added new activity indicator

--- MyVet.Prism/MyVet.Prism.iOS/AppDelegate.cs
+++ MyVet.Prism/MyVet.Prism.iOS/AppDelegate.cs
@@ -1,6 +1,7 @@
 ﻿using Foundation;
 using Prism;
 using Prism.Ioc;
+using Syncfusion.SfBusyIndicator.XForms.iOS;
 using UIKit;
 
 
@@ -24,7 +25,7 @@ namespace MyVet.Prism.iOS
             global::Xamarin.Forms.Forms.Init();
             FFImageLoading.Forms.Platform.CachedImageRenderer.Init();
             LoadApplication(new App(new iOSInitializer()));
-
+            new SfBusyIndicatorRenderer();
             return base.FinishedLaunching(app, options);
         }
     }
